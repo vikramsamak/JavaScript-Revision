027c107b005ecc0b64f798539227846e4154f5ae
vikramsamak <vikramsamak02@gmail.com>
2024-03-08 08:12:35 +0530

    Day-2 Datatypes

---
+++ DAY-2 Datatypes/index.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+let name = "Username"; //string
+
+let age = 2; //number
+
+let isLoggedin = false; //boolean
+
+let state; //undefined
+
+/*-- DATATYPES --*/
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
