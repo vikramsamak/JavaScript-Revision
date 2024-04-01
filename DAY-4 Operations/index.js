ad7b78b6cc037be8f3a4c91c08f69fe1b3a6d0fd
vikramsamak <vikramsamak02@gmail.com>
2024-04-01 08:47:08 +0530

    Day-4 Operations

---
+++ DAY-4 Operations/index.js
@@ -0,0 +1,35 @@
+//OPERATIONS
+
+let value = 3;
+let negValaue = -value;
+
+console.log(negValaue); //-3
+
+console.log(2 ** 3); // 2 raise to 3 =>8
+
+let str1 = "hello";
+let str2 = " world";
+let str3 = str1 + str2;
+
+console.log(str3); // hello world
+
+console.log("1" + 2); //12
+console.log(1 + "3"); //13
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "3"); //33
+
+console.log(+true); // 1
+console.log(+""); //0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+//num1 = 4
+//num2 = 4
+//num3 = 4
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); //101
+
